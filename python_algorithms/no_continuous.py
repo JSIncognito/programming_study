41eec01b5b1b7a12a1ea60b888ae41d1ddad5ea2
Junsu <junsoo3218@gmail.com>
2018-01-20 23:00:34 +0900

    프로그래머스 알고리즘 easy Level 1 * 피보나치 수 * 같은 숫자는 싫어 * 문자열 내 p 와 y의 개수 * 정수제곱근판별하기 * 핸드폰번호 가리기 * 스트링을 숫자로 바꾸기 * 삼각형 출력하기

---
+++ python_algorithms/no_continuous.py
@@ -0,0 +1,19 @@
+# 3. no_continuous함수는 스트링 s를 매개변수로 입력받습니다.
+#
+# s의 글자들의 순서를 유지하면서, 글자들 중 연속적으로 나타나는 아이템은 제거된 배열(파이썬은 list)을 리턴하도록 함수를 완성하세요.
+# 예를들어 다음과 같이 동작하면 됩니다.
+#
+# s가 '133303'이라면 ['1', '3', '0', '3']를 리턴
+# s가 '47330'이라면 [4, 7, 3, 0]을 리턴
+def no_continuous(s):
+    # 함수를 완성하세요
+    list = []
+    for i in s:
+        if list.count(i)<1:
+            list.append(i)
+        elif list[-1] != i:
+            list.append(i)
+    return list
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print( no_continuous( "5500999933333000" ))
